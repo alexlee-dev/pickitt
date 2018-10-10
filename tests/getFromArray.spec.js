0ab38ae532e885ee906b3e1c71232dac96eeeeb4
Alex Lee <alex@alex-lee.site>
2018-10-10 07:47:04 -0700

    ✅ Add Test for Empty Array

--- tests/getFromArray.spec.js
+++ tests/getFromArray.spec.js
@@ -2,7 +2,12 @@ const { pickitt } = require('../index')
 require('jest-matcher-one-of')
 
 const exampleArray = ['elem1', 'elem2', 'elem3']
+const emptyArray = []
 
 test('Gets an item from an example array.', () => {
   expect(pickitt(exampleArray)).toBeOneOf(['elem1', 'elem2', 'elem3'])
+})
+
+test('Returns undefined for an empty array.', () => {
+  expect(pickitt(emptyArray)).toBeUndefined()
 })
