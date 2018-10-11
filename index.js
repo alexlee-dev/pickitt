e418cd1d7927d6dca6b407cd84ae729a0c86ec67
Alex Lee <alex@alex-lee.site>
2018-10-10 18:28:24 -0700

    🔥 Remove isArray() in favor of `Array.isArray()`

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-'use strict'
-/**
- * Checks if a `group` is similar to an array: if the `group` has a length greater than 0.
- * @param {*} group The group to be checked.
- * @returns {Boolean} true | false
- */
-function isArray(group) {
-  if (
-    typeof group === 'string' ||
-    typeof group === 'number' ||
-    typeof group === 'boolean' ||
-    typeof group === 'undefined' ||
-    typeof group === 'function' ||
-    group === null
-  ) {
-    return false
-  } else {
-    return group.length > 0
-  }
-}
+"use strict";
 /**
  * Gets a random number between 0 and a `max`.
  * @param {Number} max A number as a maximum boundary.
  * @returns {Number} A number between 0 and the `max`.
  */
 function randomBetween(max) {
-  return Math.floor(Math.random() * max)
+    return Math.floor(Math.random() * max);
 }
 /**
  * Get a randomly chosen element from a group of elements.
@@ -32,18 +13,19 @@ function randomBetween(max) {
  * @returns {Element | Undefined} A randomly chosen element from the group. Returns `undefined` if the group didn't have a length greater than 0.
  */
 function pickitt(group) {
-  var array = isArray(group) ? group : undefined
-  if (array) {
-    var length_1 = array.length
-    return length_1 > 0 ? array[randomBetween(length_1)] : undefined
-  } else {
-    throw 'pickit: Function expected array, and instead got ' +
-      typeof group +
-      '.'
-  }
+    var array = Array.isArray(group) ? group : undefined;
+    if (array) {
+        var length_1 = array.length;
+        if (length_1 === 0) {
+            throw "pickitt: Array must not be empty. Please supply an array with at least one element.";
+        }
+        return length_1 > 0 ? array[randomBetween(length_1)] : undefined;
+    }
+    else {
+        throw "pickitt: Function expected array, and instead got " + typeof group + ".";
+    }
 }
 module.exports = {
-  pickitt: pickitt,
-  isArray: isArray,
-  randomBetween: randomBetween
-}
+    pickitt: pickitt,
+    randomBetween: randomBetween
+};
--- index.ts
+++ index.ts
@@ -1,24 +1,4 @@
 /**
- * Checks if a `group` is similar to an array: if the `group` has a length greater than 0.
- * @param {*} group The group to be checked.
- * @returns {Boolean} true | false
- */
-function isArray(group: any[]) {
-  if (
-    typeof group === 'string' ||
-    typeof group === 'number' ||
-    typeof group === 'boolean' ||
-    typeof group === 'undefined' ||
-    typeof group === 'function' ||
-    group === null
-  ) {
-    return false
-  } else {
-    return group.length > 0
-  }
-}
-
-/**
  * Gets a random number between 0 and a `max`.
  * @param {Number} max A number as a maximum boundary.
  * @returns {Number} A number between 0 and the `max`.
@@ -33,18 +13,22 @@ function randomBetween(max: number) {
  * @returns {Element | Undefined} A randomly chosen element from the group. Returns `undefined` if the group didn't have a length greater than 0.
  */
 function pickitt(group: any[]) {
-  const array = isArray(group) ? group : undefined
+  const array = Array.isArray(group) ? group : undefined
+
   if (array) {
     const length = array.length
 
+    if (length < 0) {
+      throw `pickitt: Array must not be empty. Please supply an array with at least one element.`
+    }
+
     return length > 0 ? array[randomBetween(length)] : undefined
   } else {
-    throw `pickit: Function expected array, and instead got ${typeof group}.`
+    throw `pickitt: Function expected array, and instead got ${typeof group}.`
   }
 }
 
 module.exports = {
   pickitt,
-  isArray,
   randomBetween
 }
