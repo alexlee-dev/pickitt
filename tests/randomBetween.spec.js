d1c19ba1b1f2a1f520c9e1d7c89c240ec1d730f3
Alex Lee <alex@alex-lee.site>
2018-10-16 19:41:34 -0700

    ✅ Group Tests Together
    Issue #10

--- tests/randomBetween.spec.js
+++ tests/randomBetween.spec.js
@@ -9,39 +9,41 @@ const exampleFunction = () => {
   return 1
 }
 
-test('Returns NaN if max is an object.', () => {
-  expect(randomBetween(object)).toBeFalsy()
-})
+describe('Type Tests for randomBetween().', () => {
+  test('Returns NaN if max is an object.', () => {
+    expect(randomBetween(object)).toBeFalsy()
+  })
 
-test('Returns NaN if max is a string with length > 0.', () => {
-  expect(randomBetween(nonEmptyString)).toBeFalsy()
-})
+  test('Returns NaN if max is a string with length > 0.', () => {
+    expect(randomBetween(nonEmptyString)).toBeFalsy()
+  })
 
-test('Returns NaN if max is an empty string.', () => {
-  expect(randomBetween(emptyString)).toBeFalsy()
-})
+  test('Returns NaN if max is an empty string.', () => {
+    expect(randomBetween(emptyString)).toBeFalsy()
+  })
 
-test('Returns a random whole number between 0 and a max if max is a number.', () => {
-  expect(randomBetween(100)).toBeGreaterThan(0)
-  expect(randomBetween(100)).toBeLessThanOrEqual(100)
-})
+  test('Returns a random whole number between 0 and a max if max is a number.', () => {
+    expect(randomBetween(100)).toBeGreaterThan(0)
+    expect(randomBetween(100)).toBeLessThanOrEqual(100)
+  })
 
-test('Returns NaN if max is a boolean = true.', () => {
-  expect(randomBetween(booleanTrue)).toBeFalsy()
-})
+  test('Returns NaN if max is a boolean = true.', () => {
+    expect(randomBetween(booleanTrue)).toBeFalsy()
+  })
 
-test('Returns NaN if max is a boolean = false.', () => {
-  expect(randomBetween(booleanFalse)).toBeFalsy()
-})
+  test('Returns NaN if max is a boolean = false.', () => {
+    expect(randomBetween(booleanFalse)).toBeFalsy()
+  })
 
-test('Returns NaN if max is a function', () => {
-  expect(randomBetween(exampleFunction)).toBeFalsy()
-})
+  test('Returns NaN if max is a function', () => {
+    expect(randomBetween(exampleFunction)).toBeFalsy()
+  })
 
-test('Returns NaN if max is undefined.', () => {
-  expect(randomBetween(undefined)).toBeFalsy()
-})
+  test('Returns NaN if max is undefined.', () => {
+    expect(randomBetween(undefined)).toBeFalsy()
+  })
 
-test('Returns NaN if group is null.', () => {
-  expect(randomBetween(null)).toBeFalsy()
+  test('Returns NaN if group is null.', () => {
+    expect(randomBetween(null)).toBeFalsy()
+  })
 })
