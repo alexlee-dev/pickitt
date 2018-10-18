31b44278e9e1843330bf046267517d93a8f60a09
Alex Lee <alex@alex-lee.site>
2018-10-17 17:12:33 -0700

    ↔️ Split Up randomBetween() and pickitt(). Make index.js only export.
    Issues #39, #40

--- src/pickitt.ts
+++ src/pickitt.ts
@@ -1,26 +1,14 @@
-/**
- * Gets a random number between 0 and a `max`.
- * @param {Number} max A number as a maximum boundary.
- * @returns {Number} A number between 0 and the `max`.
- */
-function randomBetween(max: number) {
-  return Math.floor(Math.random() * max)
-}
+import { randomBetween } from './randomBetween'
 
 /**
  * Get a randomly chosen element from a group of elements.
  * @param {*} group A group of items to pick from.
  * @returns {Element | Undefined} A randomly chosen element from the group. Returns `undefined` if the group didn't have a length greater than 0.
  */
-function pickitt(group: any[]) {
+export function pickitt(group: any[]) {
   if (Array.isArray(group)) {
     return group[randomBetween(group.length)]
   } else {
     throw `pickitt: Function expected array, and instead got ${typeof group}.`
   }
-}
-
-module.exports = {
-  pickitt,
-  randomBetween
 }
