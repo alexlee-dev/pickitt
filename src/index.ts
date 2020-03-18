20d95e8ca1fc4308b9a1e2397c2c844701492448
alexlee-dev <alex@alexlee.dev>
2020-03-18 11:54:02 -0700

    Add writeIt()

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import randItem from "./randItem";
 import readIt from "./readIt";
 import posOrNeg from "./posOrNeg";
+import writeIt from "./writeIt";
 
 export { randItem };
 export { readIt };
 export { posOrNeg };
+export { writeIt };
