ca485934ac0b44a6b62915c969f51d0efabbd32f
alexlee-dev <alex@alexlee.dev>
2020-03-18 14:18:38 -0700

    Move declaration

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,16 @@
 // Definitions by: Alex Lee <www.alexlee.dev>
 
 /**
+ * GroupItem - Item retrieved from Group using randItem().
+ */
+type GroupItem = object | [] | number | string;
+
+/**
+ * Group - Array used in randItem().
+ */
+type Group = [];
+
+/**
  * Returns a positive or negative number, to help in random positive or negative sign.
  * @returns {Number} Either -1 or 1.
  */
