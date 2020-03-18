259219c0d245228c161401a7134842acbd34770e
alexlee-dev <alex@alexlee.dev>
2020-03-18 12:04:04 -0700

    Add titleScreen()

---
+++ src/titleScreen.ts
@@ -0,0 +1,71 @@
+import boxen, { Options as boxenOptions, BorderStyle } from "boxen";
+import chalk from "chalk";
+import clear from "clear";
+import figlet from "figlet";
+
+/**
+ * Blank style applied to Boxen.
+ */
+export const blankBoxenStyle: boxenOptions = {
+  borderStyle: {
+    topLeft: " ",
+    topRight: " ",
+    bottomLeft: " ",
+    bottomRight: " ",
+    horizontal: " ",
+    vertical: " "
+  },
+  float: "center",
+  padding: { top: 0, bottom: 0, right: 1, left: 1 }
+};
+
+/**
+ * Default style applied to Boxen.
+ */
+export const defaultBoxenStyle: boxenOptions = {
+  borderColor: "magentaBright",
+  borderStyle: BorderStyle.Round,
+  float: "center",
+  padding: { top: 0, bottom: 0, right: 1, left: 1 }
+};
+
+/**
+ * Uses Figlet to transform your text to ASCII.
+ * @param {String} txt Text to be figlet-itized.
+ * @param {Object} options Options object.
+ * @returns {Promise} Resolves with text.
+ */
+const figletPromise: Function = (
+  txt: string,
+  options: object = {}
+): Promise<string> =>
+  new Promise((resolve, reject) =>
+    figlet.text(
+      txt,
+      options,
+      (error: undefined | object, result: undefined | string) => {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve(result);
+      }
+    )
+  );
+
+const titleScreen: Function = () =>
+  new Promise(async (resolve: Function, reject: Function) => {
+    try {
+      const text: string = await figletPromise("Job Applications", {
+        font: "slant"
+      });
+
+      clear();
+      console.log(boxen(chalk.blueBright(text), defaultBoxenStyle));
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+export default titleScreen;
